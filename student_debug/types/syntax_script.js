a5e60dc75f2c55432a64e086716a3c6aa00f3d9f
studentwtcc <github.classroom.wtcc@gmail.com>
2025-04-10 16:11:52 -0400

    Fixed Syntax error
    missing curly braces...now fixed

--- student_debug/types/syntax_script.js
+++ student_debug/types/syntax_script.js
@@ -1,6 +1,7 @@
 // Syntax Error Example
 function greet() {
     console.log("Hello world");
-// Missing closing brace is a syntax error
+}
+
     
 greet(); // Call the function to execute its contents
